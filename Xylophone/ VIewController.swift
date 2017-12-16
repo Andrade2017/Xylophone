51d94e8d130969b5ad6a7f8e6c232435ecebf904
Thiago Andrade <thiagoblandrade@MacBook-Air-de-Thiago.local>
2017-12-16 00:23:57 -0600

    Add all files to git hub

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
+    var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1" , "note2" , "note3" , "note4" , "note5" , "note6" , "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +22,33 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        let selectSoundNameFile: String = soundArray[sender.tag - 1]
+        print(selectSoundNameFile)
+        playSound(selectSoundNameFile)
         
     }
     
+    func playSound(_ note: String) {
+        
+        let soundURL = Bundle.main.url(forResource: note, withExtension: "wav") // signing the constante to the address where is the file to play the sound
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+        
+    }
   
 
 }
+
+// "do,try,catch" is used when there is a possibility of throw an error, so it "try" to do something and if there is a erros we can deal with it.
+
+// Another option to use those is:
+// "try! audioPlayer = AVAudioPlayer(contentsOf: soundURL!)"
+// In this case we are 100% sure that we wouldn't have any error, then we do need to catch it.
+
 
